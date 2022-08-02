675d3fff40a91fc4ee5e4926a90189e899d638a0
Guilherme Feitosa <guifcm12@gmail.com>
2022-08-02 13:22:47 -0300

    Add destroy method on product controller

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -65,8 +65,8 @@ class ProductController extends Controller
      * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Product $product)
+    public function destroy($id)
     {
-        //
+        return Product::destroy($id);
     }
 }

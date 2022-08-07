c6ff45d23e623e2079b61b46195dc2e560303aa5
Guilherme Feitosa <guifcm12@gmail.com>
2022-08-07 08:44:03 -0300

    Add register method on register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
-    //
+    public function register(Request $request, User $user){
+        $userData = $request->only('name', 'email', 'password');
+
+        $user = $user->create($userData);
+        if (! $user)
+            abort(500, 'Error to create a user');
+
+        $token = $request->user()->createToken();
+
+        return response()->json([
+            'user' => $user,
+            'token' => $token->plainTextToken
+        ]);
+    }
 }
